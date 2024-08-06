52ed9e3f0e8e9cbf1fa56f162ba145d15fc835c2
Usuario <Usuario@CPD02-M11>
2024-08-06 11:37:51 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsLoja/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsLoja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Produto = new System.Windows.Forms.TextBox();
+            this.txt_Quantidade = new System.Windows.Forms.TextBox();
+            this.txt_Preco = new System.Windows.Forms.TextBox();
+            this.bnt_Produto = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_Total = new System.Windows.Forms.Label();
+            this.bnt_novo = new System.Windows.Forms.Button();
+            this.btn_Fechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Produto:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Quantidade:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Preço unidade:";
+            // 
+            // txt_Produto
+            // 
+            this.txt_Produto.Location = new System.Drawing.Point(214, 42);
+            this.txt_Produto.Name = "txt_Produto";
+            this.txt_Produto.Size = new System.Drawing.Size(159, 20);
+            this.txt_Produto.TabIndex = 3;
+            // 
+            // txt_Quantidade
+            // 
+            this.txt_Quantidade.Location = new System.Drawing.Point(214, 74);
+            this.txt_Quantidade.Name = "txt_Quantidade";
+            this.txt_Quantidade.Size = new System.Drawing.Size(159, 20);
+            this.txt_Quantidade.TabIndex = 4;
+            // 
+            // txt_Preco
+            // 
+            this.txt_Preco.Location = new System.Drawing.Point(214, 111);
+            this.txt_Preco.Name = "txt_Preco";
+            this.txt_Preco.Size = new System.Drawing.Size(159, 20);
+            this.txt_Preco.TabIndex = 5;
+            // 
+            // bnt_Produto
+            // 
+            this.bnt_Produto.Location = new System.Drawing.Point(239, 153);
+            this.bnt_Produto.Name = "bnt_Produto";
+            this.bnt_Produto.Size = new System.Drawing.Size(75, 23);
+            this.bnt_Produto.TabIndex = 6;
+            this.bnt_Produto.Text = "Inserir produto";
+            this.bnt_Produto.UseVisualStyleBackColor = true;
+            this.bnt_Produto.Click += new System.EventHandler(this.bnt_Produto_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(117, 194);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(266, 134);
+            this.listBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(106, 352);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Total de Vendas:";
+            // 
+            // lbl_Total
+            // 
+            this.lbl_Total.AutoSize = true;
+            this.lbl_Total.Location = new System.Drawing.Point(239, 352);
+            this.lbl_Total.Name = "lbl_Total";
+            this.lbl_Total.Size = new System.Drawing.Size(42, 13);
+            this.lbl_Total.TabIndex = 9;
+            this.lbl_Total.Text = "R$0,00";
+            // 
+            // bnt_novo
+            // 
+            this.bnt_novo.Location = new System.Drawing.Point(93, 386);
+            this.bnt_novo.Name = "bnt_novo";
+            this.bnt_novo.Size = new System.Drawing.Size(75, 23);
+            this.bnt_novo.TabIndex = 10;
+            this.bnt_novo.Text = "Nova Venda";
+            this.bnt_novo.UseVisualStyleBackColor = true;
+            this.bnt_novo.Click += new System.EventHandler(this.bnt_novo_Click);
+            // 
+            // btn_Fechar
+            // 
+            this.btn_Fechar.Location = new System.Drawing.Point(242, 386);
+            this.btn_Fechar.Name = "btn_Fechar";
+            this.btn_Fechar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Fechar.TabIndex = 11;
+            this.btn_Fechar.Text = "Fechar";
+            this.btn_Fechar.UseVisualStyleBackColor = true;
+            this.btn_Fechar.Click += new System.EventHandler(this.btn_Fechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Fechar);
+            this.Controls.Add(this.bnt_novo);
+            this.Controls.Add(this.lbl_Total);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.bnt_Produto);
+            this.Controls.Add(this.txt_Preco);
+            this.Controls.Add(this.txt_Quantidade);
+            this.Controls.Add(this.txt_Produto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Produto;
+        private System.Windows.Forms.TextBox txt_Quantidade;
+        private System.Windows.Forms.TextBox txt_Preco;
+        private System.Windows.Forms.Button bnt_Produto;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_Total;
+        private System.Windows.Forms.Button bnt_novo;
+        private System.Windows.Forms.Button btn_Fechar;
+    }
+}
+
---
+++ WindowsFormsLoja/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace WindowsFormsLoja
+{
+    public partial class Form1 : Form
+    {
+        int Total;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void bnt_Produto_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(txt_Produto.Text + " / Quant:" + "" + txt_Quantidade.Text + " / R$" + txt_Preco.Text + ",00");
+            Total += int.Parse(txt_Preco.Text) * int.Parse(txt_Quantidade.Text);
+            lbl_Total.Text = Total.ToString("C");
+            
+                
+        }
+
+        private void bnt_novo_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            txt_Preco.Text = "";
+            txt_Produto.Text = "";
+            txt_Quantidade.Text = "";
+            lbl_Total.Text = "R$0,00";
+            txt_Produto.Focus();
+        }
+
+        private void btn_Fechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
